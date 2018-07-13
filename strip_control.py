53e06bedd3ecb2f086be75c01b5d2140b276d835
pgibert3 <pgibert3@gatech.edu>
2018-07-13 04:43:15 +0000

    rewrote low level control for apa102 and created a file of test functions

---
+++ strip_control.py
@@ -0,0 +1,45 @@
+import spidev
+import numpy as np
+
+
+class strip_control:
+	
+	
+	def __init__(self, num_leds, speed=3000000):
+		self.num_leds = num_leds
+		
+		sof = [0x00]*4
+		
+		ledf_lv = [0xFF]
+		ledf_clrs = [0x00]*3
+		ledf = ledf_lv + ledf_clrs
+		
+		num_byts_eof = int(np.ceil(self.num_leds / 4)) 
+		eof = [0xFF]*num_byts_eof
+		
+		self.data = np.array(sof + ledf*num_leds + eof)
+		
+		self.spi = spidev.SpiDev()
+		self.spi.open(0, 1)
+		self.spi.max_speed_hz = speed
+	
+	
+	def set_led(self, i, r, g, b, lv):
+		r_index = 4 + 4*i + 3
+		g_index = 4 + 4*i + 2
+		b_index = 4 + 4*i + 1
+		lv_index = 4 + 4*i
+		
+		self.data[r_index] = r
+		self.data[g_index] = g
+		self.data[b_index] = b
+		self.data[lv_index] = 0b11100000 | lv
+	
+	
+	def show(self):
+		self.spi.writebytes(self.data.tolist())
+		
+		
+	def set_spi_speed(speed):
+		spi.max_speed_hz(speed)
+		
---
+++ strip_util.py
@@ -0,0 +1,50 @@
+import numpy as np
+import time
+import strip_control
+
+
+NUM_LEDS = 60
+control = strip_control.strip_control(NUM_LEDS)
+
+
+def fill(r, g, b, lv=15):
+	control.set_led(np.arange(0, NUM_LEDS), r, g, b, lv)
+	control.show()
+
+
+def clear():
+	fill(0, 0, 0, 0)
+
+
+def cycle_rgb(delay_secs, lv=15):
+	while True:
+		try:
+			fill(254, 0, 0, lv)
+			time.sleep(delay_secs)
+			
+			fill(0, 254, 0, lv)
+			time.sleep(delay_secs)
+			
+			fill(0, 0, 254, lv)
+			time.sleep(delay_secs)
+			
+		except KeyboardInterrupt:
+			clear()
+			return
+	
+def strobe(delay_secs, i=np.arange(0, NUM_LEDS), r=254, g=254, b=254, lv=15):
+	while True:
+		try:
+			control.set_led(i, r, g, b, lv)
+			control.show()
+			time.sleep(delay_secs)
+			
+			clear()
+			time.sleep(delay_secs)
+			
+		except KeyboardInterrupt:
+			clear()
+			return
+
+
+clear()
