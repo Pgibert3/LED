fc05e37821718092d00e2163722519b77c01839a
paulgibert98 <paulgibert98@gmail.com>
2018-10-10 13:22:51 -0400

    Finished onset functions and added peak picking

--- src/FeatureExtractor.py
+++ src/FeatureExtractor.py
@@ -10,7 +10,7 @@ class FeatureExtractor:
 		sr -- sampling rate
 		hop_length -- new frames to read in to buffer for analysis using feature of onset_strength_multi
 		n_fft -- total frames of buffer for analysis using feature of onset_strength_multi
-		oss_buff_length -- length of oss_buffer stored for use in oss normalisation (frames)
+		oss_buff_length -- length of oss_envelope stored for use in oss normalisation (frames)
 	"""
 	def __init__(self, channels, sr=44100, hop_length=512, n_fft=2048, oss_buff_length=2500):
 		self.channels = channels
@@ -18,20 +18,21 @@ class FeatureExtractor:
 		self.hop_length = hop_length
 		self.n_fft = n_fft
 		self.oss_buff_length = oss_buff_length
-		self.oss_channels = np.zeros((oss_buf_length, len(self.channels)-1)) #Stored as a multi-demensional point for graphing
+		self.oss_env = np.zeros((oss_buf_length, len(self.channels)-1)) #Stored as a multi-demensional point for graphing
 
 		#Tracked values for onset detection
-		self.oss_min = []; #Local min of oss_buffer per channel
-		self.oss_max = []; #Local max of oss_buffer per channel
+		self.oss_min = []; #Local min of oss_envelope per channel
+		self.oss_max = []; #Local max of oss_envelope per channel
+		self.fslp = 0; #Frames since last peak
 
-	#Stores the provided array of onset strength signals in the oss_channels buffer
+	#Stores the provided array of onset strength signals in the oss_env
 	def _store_oss(self, oss):
-		self.oss_channels = np.delete(self.oss_channels, 0, axis=0)
-		self.oss_channels = np.append(self.oss_channels, oss, axis=0)
+		self.oss_env = np.delete(self.oss_env, 0, axis=0)
+		self.oss_env = np.append(self.oss_env, oss, axis=0)
 
 	#Computes the oss of a frame and stores it
 	def _oss(self, frame, n_mels=128):
-		oss_channels = onset.onset_strength_multi(
+		oss_env = onset.onset_strength_multi(
 				y=frame,
 				channels=self.channels,
 				sr=self.sr,
@@ -49,11 +50,35 @@ class FeatureExtractor:
 		self.oss_max = np.min((self.oss_min, oss), axis=0)
 		return (oss - self.oss_min) / self.oss_max
 
-	#Returns true if current frame is an onset. Implimentation based on Librosa.onset.onset_detect()
+	#Returns true if oss value represents a peak in the oss envelope. Assumes the oss being evaluated has been stored in the envelope. Implentation based on Librosa.util.peak_pic()
+	def _peak_pick(
+			self,
+			oss,
+			pre_max_c=0.03,
+			pre_avg_c=0.1,
+			wait_c=0.03,
+			delta=.07):
+		scale = self.sr // self.hop_length #scales the kwargs values to frame lengths
+		pre_max = self.oss_env[-pre_max_c*scale:]
+		pre_avg = self.oss_env[-pre_avg_c*scale:]
+		wait = wait_c*scale
+		is_peak = (
+			oss = np.max(pre_max, axis=0)
+			&& oss >= np.mean(pre_avg, axis=0) + delta
+			&& self.fslp > wait)
+		#Set the value of wait based on whether a new peak has been detected
+		if (is_peak):
+			self.fslp = wait
+		else:
+			self.fslp -= 1
+		return is_peak
+
+	#Returns true if current frame is an onset and stores the oss in the oss envelope. Implimentation based on Librosa.onset.onset_detect()
 	def is_onset(self, frame):
 		oss = self._oss(frame)
 		oss = self._normalize_oss(oss)
-		#TODO keep going
+		self._store_oss(oss)
+		return _self._peak_pick(oss) #Use default kwargs
 
 	#Uses recent data to update the tempo
 	def _update_tempo(self, frame):
